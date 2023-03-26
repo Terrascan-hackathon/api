e2ff948509963acc76e764033e3282eb5190f2af
Andrei Folfa <105888251+andreiiFolfa@users.noreply.github.com>
2023-03-26 13:31:01 +0300

    a

--- src/controllers/user/getData.ts
+++ src/controllers/user/getData.ts
@@ -30,19 +30,7 @@ const getData = async (req: Request, res: Response, next: NextFunction) => {
         const {
             location
         } = req.params;
-
-        // exec(`python3 python/ml/country_box.py ${location}`, (error, stdout, stderr) => {
-        //     if (error) {
-        //         console.log(`error: ${error.message}`);
-        //     }
-        //     else if (stderr) {
-        //         console.log(`stderr: ${stderr}`);
-        //     }
-        //     else {
-        //         console.log(stdout);
-        //     }
-        // })
-
+        
         await pythonCommand(location)
             .then(() => {
                 const pythonResponse = convertBase64(filePath);
